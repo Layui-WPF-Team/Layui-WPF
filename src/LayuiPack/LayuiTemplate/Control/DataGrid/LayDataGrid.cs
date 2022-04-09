75cde60d08a97116865e2cce9e49127c34eb89bb
ywk <1357703886@qq.com>
2022-04-09 17:32:57 +0800

    提交无数文字提示

--- src/LayuiPack/LayuiTemplate/Control/DataGrid/LayDataGrid.cs
+++ src/LayuiPack/LayuiTemplate/Control/DataGrid/LayDataGrid.cs
@@ -3,11 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace LayuiTemplate.Control
 {
     public class LayDataGrid : DataGrid
     {
+        /// <summary>
+        /// 无数据提示信息
+        /// </summary>
+        public string NoDataTips
+        {
+            get { return (string)GetValue(NoDataTipsProperty); }
+            set { SetValue(NoDataTipsProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for NoDataTips.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty NoDataTipsProperty =
+            DependencyProperty.Register("NoDataTips", typeof(string), typeof(LayDataGrid), new PropertyMetadata("暂无数据"));
+
+
     }
 }
