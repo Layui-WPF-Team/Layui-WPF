b96fc37431578a9d5ddd35345de594eeb95fdc99
wenke <1357703886@qq.com>
2022-04-19 19:08:30 +0800

    新增折叠板动画
    新增折叠板代码，优化体验以及视觉效果

--- src/LayuiPack/LayuiTemplate/Control/Expander/LayExpander.cs
+++ src/LayuiPack/LayuiTemplate/Control/Expander/LayExpander.cs
@@ -6,6 +6,8 @@ using System.Windows.Controls;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Media;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media.Animation;
 
 namespace LayuiTemplate.Control
 {
@@ -14,27 +16,86 @@ namespace LayuiTemplate.Control
     /// <para>创建者:YWK</para>
     /// <para>创建时间:2022-04-18 下午 1:31:57</para>
     /// </summary>
-    public class LayExpander: Expander
+    public class LayExpander : Expander
     {
-        public LayExpander() { 
-
-        }
-        public bool IsAutoFold
+        /// <summary>
+        /// 折叠板开关按钮
+        /// </summary>
+        private ToggleButton HeaderToggleButton;
+        /// <summary>
+        /// 折叠板展现容器
+        /// </summary>
+        private StackPanel ContentStackPanel;
+        /// <summary>
+        /// 动画容器
+        /// </summary>
+        private Grid contentAnimationGrid;
+        /// <summary>
+        /// 显示开关
+        /// </summary>
+        private bool IsShow = false;
+        public LayExpander()
         {
-            get { return (bool)GetValue(IsAutoFoldProperty); }
-            set { SetValue(IsAutoFoldProperty, value); }
+            Loaded -= LayExpander_Loaded;
+            Loaded += LayExpander_Loaded;
         }
-
-        // Using a DependencyProperty as the backing store for IsAutoFold.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty IsAutoFoldProperty =
-            DependencyProperty.Register("IsAutoFold", typeof(bool), typeof(LayExpander), new PropertyMetadata(false, Fold));
-
-        private static void Fold(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        /// <summary>
+        /// 顶部高度
+        /// </summary>
+        public double HeaderHeight
         {
-           
+            get { return (double)GetValue(HeaderHeightProperty); }
+            set { SetValue(HeaderHeightProperty, value); }
         }
-
-
+        // Using a DependencyProperty as the backing store for HeaderHeight.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty HeaderHeightProperty =
+            DependencyProperty.Register("HeaderHeight", typeof(double), typeof(LayExpander));
+        private void LayExpander_Loaded(object sender, RoutedEventArgs e)
+        {
+            HeaderToggleButton.Checked -= HeaderToggleButton_Click;
+            HeaderToggleButton.Unchecked -= HeaderToggleButton_Click;
+            HeaderToggleButton.Checked += HeaderToggleButton_Click;
+            HeaderToggleButton.Unchecked += HeaderToggleButton_Click;
+            contentAnimationGrid.SizeChanged += ContentAnimationGrid_SizeChanged;
+            if (!IsExpanded) AnimationGrid(TimeSpan.FromSeconds(0));
+        }
+        /// <summary>
+        /// 设置可视化宽度
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ContentAnimationGrid_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (IsExpanded) AnimationGrid(TimeSpan.FromSeconds(0));
+        }
+        /// <summary>
+        /// 响应当前点击状态
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void HeaderToggleButton_Click(object sender, RoutedEventArgs e)
+        {
+            IsShow = (bool)(sender as ToggleButton).IsChecked;
+            AnimationGrid(TimeSpan.FromSeconds(0.2));
+            //RefreshIsExpanded();
+        }
+        private void RefreshIsExpanded()
+        {
+            if (Parent == null) return;
+            if (Parent is LayExpanderPanel panel)
+            {
+                if (!panel.IsAutoFold) return;
+                foreach (var item in panel.Children)
+                {
+                    if (item is LayExpander expander) {
+                        if (expander != this) expander.IsExpanded = false; 
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// 顶部颜色
+        /// </summary>
         public Brush HeaderBackground
         {
             get { return (Brush)GetValue(HeaderBackgroundProperty); }
@@ -44,11 +105,26 @@ namespace LayuiTemplate.Control
         // Using a DependencyProperty as the backing store for HeaderBackground.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty HeaderBackgroundProperty =
             DependencyProperty.Register("HeaderBackground", typeof(Brush), typeof(LayExpander));
-
+        /// <summary>
+        /// 刷新动画
+        /// </summary>
+        /// <param name="time"></param>
+        private void AnimationGrid(TimeSpan time)
+        {
+            DoubleAnimation animation = new DoubleAnimation()
+            {
+                From = IsShow == true ? 0 : contentAnimationGrid.DesiredSize.Height,
+                To = IsShow == true ? contentAnimationGrid.DesiredSize.Height : 0,
+                Duration = time
+            };
+            ContentStackPanel.BeginAnimation(HeightProperty, animation);
+        }
         public override void OnApplyTemplate()
         {
             base.OnApplyTemplate();
+            HeaderToggleButton = GetTemplateChild("header") as ToggleButton;
+            ContentStackPanel = GetTemplateChild("contentPanel") as StackPanel;
+            contentAnimationGrid = GetTemplateChild("contentAnimationGrid") as Grid;
         }
-
     }
 }
---
+++ src/LayuiPack/LayuiTemplate/Control/Expander/LayExpanderPanel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace LayuiTemplate.Control
+{
+    /// <summary>
+    ///  LayExpanderPanel
+    /// <para>创建者:YWK</para>
+    /// <para>创建时间:2022-04-19 下午 5:56:27</para>
+    /// </summary>
+    public class LayExpanderPanel:StackPanel
+    {
+        private void LayExpanderPanel_Loaded(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsAutoFold
+        {
+            get { return (bool)GetValue(IsAutoFoldProperty); }
+            set { SetValue(IsAutoFoldProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsAutoFold.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsAutoFoldProperty =
+            DependencyProperty.Register("IsAutoFold", typeof(bool), typeof(LayExpanderPanel), new PropertyMetadata(false));
+    }
+}
