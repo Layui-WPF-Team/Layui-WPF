535de66d5c16519d90813a927a27e77ae0eccf09
Coolkeke <37786276+Coolkeke@users.noreply.github.com>
2022-10-23 13:06:50 +0800

    新增特殊文本控件（原始文本增强）

---
+++ src/LayuiPack/LayuiTemplate/Controls/TextBlock/LayTextBlock.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace LayuiTemplate.Controls
+{
+    /// <summary>
+    /// 文本控件
+    /// </summary>
+    public class LayTextBlock : TextBlock
+    {
+        /// <summary>
+        /// 文字是否已被裁剪
+        /// </summary>
+        public bool IsTrimming
+        {
+            get { return (bool)GetValue(IsTrimmingProperty); }
+            set { SetValue(IsTrimmingProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsTrimming.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsTrimmingProperty =
+            DependencyProperty.Register("IsTrimming", typeof(bool), typeof(LayTextBlock));
+
+        /// <summary>
+        /// 获取当前文字是否已被裁剪
+        /// </summary>
+        /// <returns></returns>
+        private bool GetIsTrimming()
+        {
+            if (TextTrimming == TextTrimming.None) return false;
+            if (TextTrimming == TextTrimming.WordEllipsis)
+            {
+                var size = MeasureOverride(new Size(DesiredSize.Height, double.MaxValue));
+                MeasureOverride(DesiredSize);
+                if (size.Height > DesiredSize.Height) return true;
+            }
+            if (TextTrimming == TextTrimming.CharacterEllipsis)
+            {
+                var size = MeasureOverride(new Size(double.MaxValue, DesiredSize.Height));
+                MeasureOverride(DesiredSize);
+                if (size.Width > DesiredSize.Width) return true;
+            }
+            return false;
+        }
+
+        protected override Geometry GetLayoutClip(Size layoutSlotSize)
+        {
+            IsTrimming = GetIsTrimming();
+            return base.GetLayoutClip(layoutSlotSize);
+        }
+    }
+}
--- src/LayuiPack/LayuiTemplate/Extend/Image/LayImageHelper.cs
+++ src/LayuiPack/LayuiTemplate/Extend/Image/LayImageHelper.cs
@@ -12,6 +12,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 
 namespace LayuiTemplate.Extend
 {
