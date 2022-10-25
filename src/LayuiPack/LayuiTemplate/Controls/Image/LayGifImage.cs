7f7144e97891469dac68dc23457619b3efcd24ee
Coolkeke <37786276+Coolkeke@users.noreply.github.com>
2022-10-25 23:09:03 +0800

    修复GIF控件内存溢出问题

--- src/LayuiPack/LayuiTemplate/Controls/Image/LayGifImage.cs
+++ src/LayuiPack/LayuiTemplate/Controls/Image/LayGifImage.cs
@@ -3,6 +3,7 @@ using LayuiTemplate.Tools;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -14,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Threading;
+using Image = System.Windows.Controls.Image;
 
 namespace LayuiTemplate.Controls
 {
@@ -22,23 +24,9 @@ namespace LayuiTemplate.Controls
     /// <para>创建者:YWK</para>
     /// <para>创建时间:2022-07-19 上午 9:38:52</para>
     /// </summary>
-    public class LayGifImage : System.Windows.Controls.Control
+    public class LayGifImage : System.Windows.Controls.Control, IDisposable
     {
-        static LayGifImage()
-        {
-            VisibilityProperty.OverrideMetadata(typeof(LayGifImage), new PropertyMetadata(default(Visibility), OnVisibilityChanged));
-        }
-
-        private static void OnVisibilityChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            LayGifImage layGif = d as LayGifImage;
-            layGif.Refresh();
-        }
-
-        /// <summary>
-        /// 图片加载容器
-        /// </summary>
-        private ImageBrush PART_Image;
+        private Image PART_Image;
         /// <summary>
         /// gif动画的System.Drawing.Bitmap
         /// </summary>
@@ -47,6 +35,8 @@ namespace LayuiTemplate.Controls
         /// 用于显示每一帧的BitmapSource
         /// </summary>
         private BitmapSource bitmapSource;
+        private bool disposedValue;
+
         [Bindable(true)]
         public ImageSource Source
         {
@@ -57,10 +47,12 @@ namespace LayuiTemplate.Controls
         public static readonly DependencyProperty SourceProperty =
             DependencyProperty.Register("Source", typeof(ImageSource), typeof(LayGifImage), new PropertyMetadata(OnSourceChanged));
 
-        private static void OnSourceChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        private async static void OnSourceChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             LayGifImage layGif = d as LayGifImage;
-            layGif.Refresh();
+            if (!layGif.IsLoaded) return;
+            if (e.NewValue == e.OldValue) return;
+            await layGif.RefreshAsync();
         }
 
 
@@ -87,29 +79,38 @@ namespace LayuiTemplate.Controls
         public static readonly DependencyProperty StretchProperty =
             DependencyProperty.Register("Stretch", typeof(Stretch), typeof(LayGifImage), new PropertyMetadata(Stretch.Fill));
 
-        private void Refresh()
+        private Task RefreshAsync()
         {
-            if (!IsLoaded) return;
-            Dispatcher.Invoke(async () =>
-            {
-                if (Source != null)
-                {
-                    gifBitmap = await LayImageHelper.GetBitmapAsync(Source);
-                    bitmapSource = GetBitmapSource();
-                    PART_Image.ImageSource = bitmapSource;
-                    StopAnimate();
-                    StartAnimate();
-                }
-            });
+            return Task.Run(() =>
+           {
+               if (PART_Image != null)
+               {
+                   Dispatcher.Invoke(async () =>
+                   {
+                       if (gifBitmap != null) {
+                           StopAnimate();
+                           gifBitmap?.Dispose();
+                       }
+                       this.gifBitmap = await LayImageHelper.GetBitmapAsync(Source);
+                       this.bitmapSource = this.GetBitmapSource();
+                       this.PART_Image.Source = this.bitmapSource;
+                       StartAnimate();
+                   });
+               }
+           });
 
         }
+        /// <summary>
+        /// 从System.Drawing.Bitmap中获得用于显示的那一帧图像的BitmapSource
+        /// </summary>
+        /// <returns></returns>
         private BitmapSource GetBitmapSource()
         {
             IntPtr handle = IntPtr.Zero;
             try
             {
-                handle = gifBitmap.GetHbitmap();
-                bitmapSource = Imaging.CreateBitmapSourceFromHBitmap(handle, IntPtr.Zero, System.Windows.Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+                handle = this.gifBitmap.GetHbitmap();
+                this.bitmapSource = Imaging.CreateBitmapSourceFromHBitmap(handle, IntPtr.Zero, System.Windows.Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
             }
             finally
             {
@@ -118,36 +119,46 @@ namespace LayuiTemplate.Controls
                     LayImageHelper.DeleteObject(handle);
                 }
             }
-            return bitmapSource;
+            return this.bitmapSource;
         }
+
+        /// <summary>
+        /// Start
+        /// </summary>
+        public void StartAnimate()
+        {
+           ImageAnimator.Animate(this.gifBitmap, this.OnFrameChanged);
+        }
+
+        /// <summary>
+        /// Stop
+        /// </summary>
+        public void StopAnimate()
+        {
+          ImageAnimator.StopAnimate(this.gifBitmap, this.OnFrameChanged);
+        }
+
+        /// <summary>
+        /// 帧处理
+        /// </summary>
         private void OnFrameChanged(object sender, EventArgs e)
         {
             Dispatcher.BeginInvoke(DispatcherPriority.Normal, new Action(() =>
-        {
-            System.Drawing.ImageAnimator.UpdateFrames(); // 更新到下一帧
-            if (PART_Image.ImageSource != null)
             {
-                PART_Image.ImageSource.Freeze();
-            }
-
-            PART_Image.ImageSource = GetBitmapSource();
-            InvalidateVisual();
-        }));
-        }
-        public void StartAnimate()
-        {
-            if (gifBitmap != null)
-                System.Drawing.ImageAnimator.Animate(gifBitmap, OnFrameChanged);
-        }
-        public void StopAnimate()
-        {
-            if (gifBitmap != null)
-                System.Drawing.ImageAnimator.StopAnimate(gifBitmap, OnFrameChanged);
+                ImageAnimator.UpdateFrames(); // 更新到下一帧
+                if (this.bitmapSource != null)
+                {
+                    this.bitmapSource.Freeze();
+                }
+                this.bitmapSource = this.GetBitmapSource();
+                this.PART_Image.Source = this.bitmapSource;
+                this.InvalidateVisual();
+            }));
         }
         public override void OnApplyTemplate()
         {
             base.OnApplyTemplate();
-            PART_Image = GetTemplateChild("PART_Image") as ImageBrush;
+            PART_Image = GetTemplateChild("PART_Image") as Image;
             Loaded -= LayGifImage_Loaded;
             Loaded += LayGifImage_Loaded;
             Unloaded -= LayGifImage_Unloaded;
@@ -159,9 +170,39 @@ namespace LayuiTemplate.Controls
             StopAnimate();
         }
 
-        private void LayGifImage_Loaded(object sender, RoutedEventArgs e)
+        private async void LayGifImage_Loaded(object sender, RoutedEventArgs e)
         {
-            Refresh();
+            await RefreshAsync();
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: 释放托管状态(托管对象)
+                }
+                StopAnimate();
+                gifBitmap?.Dispose();
+                // TODO: 释放未托管的资源(未托管的对象)并重写终结器
+                // TODO: 将大型字段设置为 null
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: 仅当“Dispose(bool disposing)”拥有用于释放未托管资源的代码时才替代终结器
+        ~LayGifImage()
+        {
+            // 不要更改此代码。请将清理代码放入“Dispose(bool disposing)”方法中
+            Dispose(disposing: false);
+        }
+
+        public void Dispose()
+        {
+            // 不要更改此代码。请将清理代码放入“Dispose(bool disposing)”方法中
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
         }
     }
 }
