ed34ac08752683e47c1d38014c4ff81f99c32044
Coolkeke <37786276+Coolkeke@users.noreply.github.com>
2022-12-23 00:17:37 +0800

    完成右键菜单

---
+++ src/LayuiPack/LayuiTemplate/Controls/Menu/LayContextMenu.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace LayuiTemplate.Controls
+{
+    public class LayContextMenu: ContextMenu
+    {
+        /// <summary>
+        /// 重写自定义指定项子控件
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        protected override bool IsItemItsOwnContainerOverride(object item)
+        {
+            return item is LayMenuItem;
+        }
+        /// <summary>
+        /// 抓取指定项控件并返回定制项控件
+        /// </summary>
+        /// <returns></returns>
+        protected override DependencyObject GetContainerForItemOverride()
+        {
+            return new LayMenuItem();
+        }
+    }
+}
