0b40002a2eb11fa0d0e0f10086071b7eb8f8687c
Coolkeke <37786276+Coolkeke@users.noreply.github.com>
2023-01-12 00:46:08 +0800

    完成窗体样式，修复输入框BUG
    新增Window窗体样式，修复输入框滚动条BUG

--- src/LayuiPack/LayUI.Wpf/Controls/Window/LayWindow.cs
+++ src/LayuiPack/LayUI.Wpf/Controls/Window/LayWindow.cs
@@ -7,18 +7,96 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shell;
 
 namespace LayUI.Wpf.Controls
 {
+    [TemplatePart(Name = "PART_CloseWindowButton", Type = typeof(Button))]
+    [TemplatePart(Name = "PART_MaxWindowButton", Type = typeof(Button))]
+    [TemplatePart(Name = "PART_MinWindowButton", Type = typeof(Button))]
     public class LayWindow : Window
     {
+        /// <summary>
+        /// 关闭窗体
+        /// </summary>
+        private Button PART_CloseWindowButton = null;
+        /// <summary>
+        /// 窗体缩放
+        /// </summary>
+        private Button PART_MaxWindowButton = null;
+        /// <summary>
+        /// 最小化窗体
+        /// </summary>
+        private Button PART_MinWindowButton = null;
+
         /// <summary>
         /// 设置重写默认样式
         /// </summary>
         static LayWindow()
         {
-            StyleProperty.OverrideMetadata(typeof(LayWindow), new FrameworkPropertyMetadata(LayResourceHelper.GetStyle(nameof(LayWindow)+"Style")));
+            StyleProperty.OverrideMetadata(typeof(LayWindow), new FrameworkPropertyMetadata(LayResourceHelper.GetStyle(nameof(LayWindow) + "Style")));
         }
+
+
+        /// <summary>
+        /// 顶部内容
+        /// </summary>
+        [Bindable(true)]
+        public object HeaderContent
+        {
+            get { return (object)GetValue(HeaderContentProperty); }
+            set { SetValue(HeaderContentProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for HeaderContent.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty HeaderContentProperty =
+            DependencyProperty.Register("HeaderContent", typeof(object), typeof(LayWindow));
+
+
+        /// <summary>
+        /// 头部标题栏文字颜色
+        /// </summary>
+        [Bindable(true)]
+        public Brush HeaderForeground
+        {
+            get { return (Brush)GetValue(HeaderForegroundProperty); }
+            set { SetValue(HeaderForegroundProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for HeaderForeground.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty HeaderForegroundProperty =
+            DependencyProperty.Register("HeaderForeground", typeof(Brush), typeof(LayWindow), new PropertyMetadata(Brushes.White));
+
+
+        /// <summary>
+        /// 头部标题栏背景色
+        /// </summary>
+        [Bindable(true)]
+        public Brush HeaderBackground
+        {
+            get { return (Brush)GetValue(HeaderBackgroundProperty); }
+            set { SetValue(HeaderBackgroundProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty HeaderBackgroundProperty =
+            DependencyProperty.Register("HeaderBackground", typeof(Brush), typeof(LayWindow));
+        /// <summary>
+        /// 是否显示头部标题栏
+        /// </summary>
+        [Bindable(true)]
+        public bool IsShowHeader
+        {
+            get { return (bool)GetValue(IsShowHeaderProperty); }
+            set { SetValue(IsShowHeaderProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsShowHeader.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsShowHeaderProperty =
+            DependencyProperty.Register("IsShowHeader", typeof(bool), typeof(LayWindow), new PropertyMetadata(false));
+
         /// <summary>
         /// 动画类型
         /// </summary>
@@ -32,13 +110,53 @@ namespace LayUI.Wpf.Controls
         // Using a DependencyProperty as the backing store for Type.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty TypeProperty =
             DependencyProperty.Register("Type", typeof(AnimationType), typeof(LayWindow), new PropertyMetadata(AnimationType.Default));
-        protected override void OnInitialized(EventArgs e)
+
+        public override void OnApplyTemplate()
         {
-            base.OnInitialized(e);
+            base.OnApplyTemplate();
+            PART_CloseWindowButton = GetTemplateChild("PART_CloseWindowButton") as Button;
+            PART_MaxWindowButton = GetTemplateChild("PART_MaxWindowButton") as Button;
+            PART_MinWindowButton = GetTemplateChild("PART_MinWindowButton") as Button;
+            if (PART_MaxWindowButton != null && PART_MinWindowButton != null && PART_CloseWindowButton != null)
+            {
+                PART_MaxWindowButton.Click -= PART_MaxWindowButton_Click;
+                PART_MinWindowButton.Click -= PART_MinWindowButton_Click;
+                PART_CloseWindowButton.Click -= PART_CloseWindowButton_Click;
+                PART_MaxWindowButton.Click += PART_MaxWindowButton_Click;
+                PART_MinWindowButton.Click += PART_MinWindowButton_Click;
+                PART_CloseWindowButton.Click += PART_CloseWindowButton_Click;
+            }
         }
-        protected override void OnClosing(CancelEventArgs e)
+
+        private void PART_CloseWindowButton_Click(object sender, RoutedEventArgs e)
         {
-            base.OnClosing(e);
+            Close();
+        }
+
+        private void PART_MinWindowButton_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void PART_MaxWindowButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Normal)
+            {
+                WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                WindowState = WindowState.Normal;
+            }
+        }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+            if (SizeToContent == SizeToContent.WidthAndHeight && WindowChrome.GetWindowChrome(this) != null)
+            {
+                InvalidateMeasure();
+            }
         }
     }
 }
--- src/LayuiPack/LayuiApp/Views/DialogWindowBase.xaml.cs
+++ src/LayuiPack/LayuiApp/Views/DialogWindowBase.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Shell;
 
 namespace LayuiApp.Views
 {
