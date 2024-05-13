41ba6cc4213c84384ca89bafdf99abab1446ff1f
Coolkeke <37786276+Coolkeke@users.noreply.github.com>
2024-05-13 17:16:34 +0800

    创建 LayCodeTextBox.cs

---
+++ src/LayuiPack/LayUI.Wpf/Controls/TextBox/LayCodeTextBox.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace LayUI.Wpf.Controls
+{
+    /// <summary>
+    /// 代码高亮输入框
+    /// </summary>
+    public class LayCodeTextBox: Control
+    { 
+        public object Text
+        {
+            get { return (object)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Text.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(object), typeof(LayCodeTextBox)); 
+
+    }
+}
